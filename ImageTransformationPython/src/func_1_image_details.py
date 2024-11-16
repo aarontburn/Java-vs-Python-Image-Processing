66a14cddc4f50ca9cf1bf500f2798c1bbfe6d663
Aaron <aarontburnham@hotmail.com>
2024-11-15 17:10:54 -0800

    changese

--- ImageTransformationPython/src/func_1_image_details.py
+++ ImageTransformationPython/src/func_1_image_details.py
@@ -4,6 +4,9 @@ import os
 
 from PIL import Image
 
+IMAGE_FILE_KEY: str = 'image_file'
+ERROR_KEY: str = 'error'
+
 
 def handle_request(event, context = None):
     """
@@ -29,8 +32,12 @@ def handle_request(event, context = None):
     :param context: 
     :returns:       A dictionary containing the response data.
     """
+    if IMAGE_FILE_KEY not in event:
+        return {ERROR_KEY: "Missing key-value pair associated with " + IMAGE_FILE_KEY }
+
+
     try:
-        with Image.open(io.BytesIO(base64.decodebytes(event['image_file']))) as img:
+        with Image.open(io.BytesIO(base64.decodebytes(event[IMAGE_FILE_KEY]))) as img:
             return {
                 'region': os.environ['AWS_REGION'] if 'AWS_REGION' in os.environ else 'NO_REGION_DATA',
                 'height': img.height,
@@ -39,10 +46,10 @@ def handle_request(event, context = None):
                 'has_transparency_data': 1 if img.has_transparency_data else 0
             }
     except Exception as e:
-        return {'error': str(e)}
+        return {ERROR_KEY: str(e)}
 
     # This might be unreachable
-    return {'error': "Unknown error processing file."}
+    return {ERROR_KEY: "Unknown error processing file."}
 
 
 # Some testing code.
@@ -50,5 +57,5 @@ if __name__ == '__main__':
     print("\nPWD: " + os.getcwd())
     image_name = 'sample image.jpg'
     with open('../sample images/' + image_name, 'rb') as f:
-        event_obj = {'image_file': base64.b64encode(f.read())}
+        event_obj = {IMAGE_FILE_KEY: base64.b64encode(f.read())}
         print(handle_request(event_obj))
--- ImageTransformationPython/src/func_5_image_brightness.py
+++ ImageTransformationPython/src/func_5_image_brightness.py
@@ -1,16 +1,58 @@
 import base64
 import os
+import io
 
+from PIL import Image, ImageEnhance
 
+ERROR_KEY: str = 'error'
+IMAGE_FILE_KEY: str = 'image_file'
+BRIGHTNESS_KEY: float = 'brightness_delta'
+BRIGHTNESS_BOUNDS: tuple[int, int] = (0, 5) # Maximum value is 5 times the original brightness? Can be changed.
 
 def handle_request(event, context = None):
-    pass
+    if IMAGE_FILE_KEY not in event:
+        return {ERROR_KEY: "Missing key-value pair associated with " + IMAGE_FILE_KEY }
+    
+    brightness_delta = 1
+    if BRIGHTNESS_KEY not in event:
+        return {ERROR_KEY: "Missing key-value pair associated with " + BRIGHTNESS_KEY }
+    else:
+        try:
+            brightness_delta = float(event[BRIGHTNESS_KEY])
+            if brightness_delta < BRIGHTNESS_BOUNDS[0] or brightness_delta > BRIGHTNESS_BOUNDS[1]:
+                return {ERROR_KEY: f"'{BRIGHTNESS_KEY}' is out-of-bounds \
+                        ({BRIGHTNESS_BOUNDS[0]}-{BRIGHTNESS_BOUNDS[1]}): {brightness_delta}"}
+
+        except:
+            return {ERROR_KEY: f"'{BRIGHTNESS_KEY}' is not parsable as a float."}
+
+
+    try:
+        with Image.open(io.BytesIO(base64.decodebytes(event[IMAGE_FILE_KEY]))) as img:
+            enhancer = ImageEnhance.Brightness(img)
+            img = enhancer.enhance(brightness_delta)
+
+            
+
+            return {
+                "args": {BRIGHTNESS_KEY: event[BRIGHTNESS_KEY]},
+                IMAGE_FILE_KEY: base64.encodebytes(img.tobytes())
+            }
+
+
+
+    except Exception as e:
+        return {ERROR_KEY: str(e)}
 
 
 # Some testing code.
 if __name__ == '__main__':
     print("\nPWD: " + os.getcwd())
-    image_name = 'github-logo.png'
-    with open('../sample images/' + image_name, 'rb') as f:
-        event_obj = {'image_file': base64.b64encode(f.read())}
+    image_name = 'sample image.jpg'
+    with open('./sample images/' + image_name, 'rb') as f:
+        event_obj = {IMAGE_FILE_KEY: base64.b64encode(f.read()), BRIGHTNESS_KEY: 1}
         print(handle_request(event_obj))
+
+
+        with open('test output.jpg', "wb") as w:
+            w.write(handle_request(event_obj)[IMAGE_FILE_KEY])
